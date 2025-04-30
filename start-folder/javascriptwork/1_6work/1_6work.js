3dab72ec23cfd3fcdb30b6a0c0d4f9c906ccca4e
yokose <yuk3218@gmail.com>
2025-04-30 13:56:27 +0900

    少し変更

--- start-folder/javascriptwork/1_6work/1_6work.js
+++ start-folder/javascriptwork/1_6work/1_6work.js
@@ -1,11 +1,11 @@
-console.log("🍽️ お客さんが注文しました");
+console.log("お客さんが注文しました");
 
 // ラーメンを作る関数
 function makeRamen() {
   return new Promise((resolve) => {
-    console.log("👨‍🍳 シェフ：先にラーメンを作り始めます...");
+    console.log("シェフ：先にラーメンを作り始めます...");
     setTimeout(() => {
-      console.log("✅ ラーメン完成！");
+      console.log(" ラーメン完成！");
       resolve("ラーメン");
     }, 3000);
   });
@@ -14,9 +14,9 @@ function makeRamen() {
 // 餃子を作る関数
 function makeGyoza() {
   return new Promise((resolve) => {
-    console.log("👨‍🍳 シェフ：後に餃子を作り始めます...");
+    console.log("シェフ：後に餃子を作り始めます...");
     setTimeout(() => {
-      console.log("✅ 餃子完成！");
+      console.log(" 餃子完成！");
       resolve("餃子");
     }, 2000);
   });
@@ -25,8 +25,8 @@ function makeGyoza() {
 // 並列で作って、終わったら配膳
 async function serveCustomer() {
   await Promise.all([makeRamen(), makeGyoza()]);
-  console.log(`🍱 シェフ：全てできたので配膳します！`);
+  console.log(`シェフ：全てできたので配膳します。`);
 }
 
-console.log("🍽️ お客さんがラーメンと餃子を注文しました");
+console.log("お客さんがラーメンと餃子を注文しました");
 serveCustomer();
